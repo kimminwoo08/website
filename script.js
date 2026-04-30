ba4c80939c1a4fae01c07150a9df0ec5f3fcc15e
kimminwoo08 <kim080402@naver.com>
2026-05-01 00:06:23 +0900

    update

--- script.js
+++ script.js
@@ -69,3 +69,16 @@ if (container) {
     });
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".menu a");
+  const currentPage = window.location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
